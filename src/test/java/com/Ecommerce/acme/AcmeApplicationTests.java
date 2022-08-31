b5bb86dc304eb08ed1e148321792f5c7542a6c55
Sheerin Banu <sheemuzz@outlook.com>
2022-08-31 15:51:04 +0200

    first commit

---
+++ src/test/java/com/Ecommerce/acme/AcmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ecommerce.acme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
