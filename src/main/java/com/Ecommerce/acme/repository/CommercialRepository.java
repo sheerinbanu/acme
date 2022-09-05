774da1c4b920a1d0cc8e164d13493ab9dfbec687
habibouomar <habibou.omar@gmail.com>
2022-09-05 11:44:19 +0200

    add modification repository

--- src/main/java/com/Ecommerce/acme/repository/CommercialRepository.java
+++ src/main/java/com/Ecommerce/acme/repository/CommercialRepository.java
@@ -1,11 +1,12 @@
 package com.Ecommerce.acme.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Ecommerce.acme.model.Commercial;
 
 @Repository
-public interface CommercialRepository extends CrudRepository<Commercial, Integer>{
+public interface CommercialRepository extends JpaRepository<Commercial, Integer>{
 
 }
--- src/main/java/com/Ecommerce/acme/repository/SelectionnerRepository.java
+++ src/main/java/com/Ecommerce/acme/repository/SelectionnerRepository.java
@@ -1,7 +1,6 @@
 package com.Ecommerce.acme.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
 import com.Ecommerce.acme.model.Selectionner;
 
 public interface SelectionnerRepository extends JpaRepository<Selectionner,Integer> {
