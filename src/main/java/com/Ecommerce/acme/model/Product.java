8e87558216a034c33633997fd0e396170b0e2143
Sheerin Banu <sheemuzz@outlook.com>
2022-09-09 17:47:15 +0200

    display categories

--- src/main/java/com/Ecommerce/acme/model/Product.java
+++ src/main/java/com/Ecommerce/acme/model/Product.java
@@ -1,12 +1,16 @@
 package com.Ecommerce.acme.model;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import lombok.Data;
 
@@ -21,4 +25,9 @@ public class Product {
 	private String name;
 	private float unit_price;
 	private String picture;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "id_category", referencedColumnName= "id_category", insertable = false, updatable = false)
+	//@Fetch(FetchMode.JOIN)
+	private Category category;
 }
