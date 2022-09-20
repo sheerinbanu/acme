89ff24ccb4969e40cc1204292f182a13d6554842
Toufik <webtoufik@gmail.com>
2022-09-20 11:02:48 +0200

    delete all on master

--- src/main/java/com/Ecommerce/acme/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Ecommerce.acme.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Ecommerce.acme.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Integer>{
-
-}
--- src/main/java/com/Ecommerce/acme/repository/ComposeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Ecommerce.acme.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.Ecommerce.acme.model.Compose;
-
-public interface ComposeRepository extends JpaRepository<Compose,Integer> {
-
-}
